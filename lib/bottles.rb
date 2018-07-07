3493a571234d01bcfe9911e20497800f595be09d
Dariusz <pixelexcel@gmail.com>
2018-07-07 10:53:56 +0200

    refactor implementaion for 3rd test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,7 @@
 class Bottles
   def verse(count)
+    return "#{count} bottles of beer on the wall, #{count} bottles of beer."\
+    "Take one down and pass it around, #{count - 1} bottle of beer on the wall." if count == 2
     "#{count} bottles of beer on the wall, #{count} bottles of beer."\
     "Take one down and pass it around, #{count - 1} bottles of beer on the wall."
   end
