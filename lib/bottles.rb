39dca6ba8db2d358ad4e61bdf498a3aaf1b9dba8
Dariusz <pixelexcel@gmail.com>
2018-07-07 11:22:01 +0200

    change implementation for 5th test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -6,6 +6,9 @@ class Bottles
     elsif count == 1
       '1 bottle of beer on the wall, 1 bottle of beer.'\
       'Take it down and pass it around, no more bottles of beer on the wall.'
+    elsif count.zero?
+      'No more bottles of beer on the wall, no more bottles of beer.'\
+      'Go to the store and buy some more, 99 bottles of beer on the wall.'
     else
       "#{count} bottles of beer on the wall, #{count} bottles of beer."\
       "Take one down and pass it around, #{count - 1} bottles of beer on the wall."
