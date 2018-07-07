8dc3c29920d56013b8380c2b92fdf68cffc25b0b
Dariusz <pixelexcel@gmail.com>
2018-07-07 10:32:33 +0200

    add first test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,7 @@
 class Bottles
 
+  def verse(count)
+    "99 bottles of beer on the wall, #{count} bottles of beer."\
+    "Take one down and pass it around, #{count - 1} bottles of beer on the wall."
+  end
 end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -3,4 +3,9 @@ require_relative '../lib/bottles'
 
 RSpec.describe Bottles do
 
+
+  it "it puts 99th verse of song '99 bottles'" do
+    expect(subject.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer.'\
+'Take one down and pass it around, 98 bottles of beer on the wall.'
+  end
 end
