476bc6c5e7e45bc33a379902126152148210b1c1
Dariusz <pixelexcel@gmail.com>
2018-07-07 11:17:30 +0200

    add 5th test

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -20,6 +20,10 @@ RSpec.describe Bottles do
   it "it puts verse of song '99 bottles' with 1 bottle" do
     expect(subject.verse(1)).to eq '1 bottle of beer on the wall, 1 bottle of beer.'\
     'Take it down and pass it around, no more bottles of beer on the wall.'
+  end
 
+  it "it puts last verse of song '99 bottles'" do
+    expect(subject.verse(1)).to eq 'No more bottles of beer on the wall, no more bottles of beer.'\
+    'Go to the store and buy some more, 99 bottles of beer on the wall.'
   end
 end
