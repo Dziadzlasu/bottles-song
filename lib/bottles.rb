c28b8ac3d9ab49fcb73563a0dc2170e77b5f0f8b
Dariusz <pixelexcel@gmail.com>
2018-07-07 10:41:07 +0200

    fix missing interpolation

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,7 @@
 class Bottles
 
   def verse(count)
-    "99 bottles of beer on the wall, #{count} bottles of beer."\
+    "#{count} bottles of beer on the wall, #{count} bottles of beer."\
     "Take one down and pass it around, #{count - 1} bottles of beer on the wall."
   end
 end
