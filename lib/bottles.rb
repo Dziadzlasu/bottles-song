61374a4cb8645bcf1d0fc6c488fbe0765ab745d6
Dariusz <pixelexcel@gmail.com>
2018-07-07 11:15:13 +0200

    add implementation for 4th test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,14 @@
 class Bottles
   def verse(count)
-    return "#{count} bottles of beer on the wall, #{count} bottles of beer."\
-    "Take one down and pass it around, #{count - 1} bottle of beer on the wall." if count == 2
-    "#{count} bottles of beer on the wall, #{count} bottles of beer."\
-    "Take one down and pass it around, #{count - 1} bottles of beer on the wall."
+    if count == 2
+      "#{count} bottles of beer on the wall, #{count} bottles of beer."\
+      "Take one down and pass it around, #{count - 1} bottle of beer on the wall."
+    elsif count == 1
+      '1 bottle of beer on the wall, 1 bottle of beer.'\
+      'Take it down and pass it around, no more bottles of beer on the wall.'
+    else
+      "#{count} bottles of beer on the wall, #{count} bottles of beer."\
+      "Take one down and pass it around, #{count - 1} bottles of beer on the wall."
+    end
   end
 end
