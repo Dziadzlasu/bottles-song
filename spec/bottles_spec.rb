5f6ed4cd93d8a238557dcfcaf6f09eb0a8a0d332
Dariusz <pixelexcel@gmail.com>
2018-07-07 10:59:43 +0200

    fix tests decriptions

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -2,18 +2,19 @@ require 'spec_helper'
 require_relative '../lib/bottles'
 
 RSpec.describe Bottles do
-  it "it puts 99th verse of song '99 bottles'" do
+  it "it puts verse of song '99 bottles with 99 bottles'" do
     expect(subject.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer.'\
     'Take one down and pass it around, 98 bottles of beer on the wall.'
   end
 
-  it "it puts 89th verse of song '99 bottles'" do
+  it "it puts verse of song '99 bottles with 89 bottles" do
     expect(subject.verse(89)).to eq '89 bottles of beer on the wall, 89 bottles of beer.'\
     'Take one down and pass it around, 88 bottles of beer on the wall.'
   end
 
-  it "it puts 89th verse of song '99 bottles'" do
+  it "it puts verse of song '99 bottles with 2 bottles" do
     expect(subject.verse(2)).to eq '2 bottles of beer on the wall, 2 bottles of beer.'\
     'Take one down and pass it around, 1 bottle of beer on the wall.'
   end
+
 end
