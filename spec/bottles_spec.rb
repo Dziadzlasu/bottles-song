590049addce9b16db2a19be723bbf9498808ed7e
Dariusz <pixelexcel@gmail.com>
2018-07-07 10:45:43 +0200

    add 2nd test passing and code cleanup

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 require_relative '../lib/bottles'
 
 RSpec.describe Bottles do
-
-
   it "it puts 99th verse of song '99 bottles'" do
     expect(subject.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer.'\
-'Take one down and pass it around, 98 bottles of beer on the wall.'
+    'Take one down and pass it around, 98 bottles of beer on the wall.'
+  end
+
+  it "it puts 89th verse of song '99 bottles'" do
+    expect(subject.verse(89)).to eq '89 bottles of beer on the wall, 89 bottles of beer.'\
+    'Take one down and pass it around, 88 bottles of beer on the wall.'
   end
 end
