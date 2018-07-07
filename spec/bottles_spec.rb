a9510018469bc5f99e530f9e5c2ecb0c7817ffee
Dariusz <pixelexcel@gmail.com>
2018-07-07 11:25:53 +0200

    add 6th test

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -26,4 +26,11 @@ RSpec.describe Bottles do
     expect(subject.verse(0)).to eq 'No more bottles of beer on the wall, no more bottles of beer.'\
     'Go to the store and buy some more, 99 bottles of beer on the wall.'
   end
+
+  it "it puts verses of song '99 bottles' with number of bottles for given range" do
+    expect(subject.verse(99, 98)).to eq '99 bottles of beer on the wall, 99 bottles of beer.'\
+    'Take one down and pass it around, 98 bottles of beer on the wall.'\
+    '98 bottles of beer on the wall, 98 bottles of beer.'\
+    'Take one down and pass it around, 97 bottles of beer on the wall.'
+  end
 end
